da90aaa9065617d1a66352cdc4a9ca671d509789
Lucas Calíope <lucascaliope2010@gmail.com>
2026-09-05 09:42:42 -0300

    feat: adiciona tipos Ator, FilmeCredito e AvaliacaoUsuario

--- types/tmdb.ts
+++ types/tmdb.ts
@@ -20,3 +20,36 @@ export interface RespostaTMDB {
   total_pages: number
   total_results: number
 }
+
+export interface Ator {
+  id: number
+  name: string
+  profile_path: string | null
+  known_for_department: string
+  popularity: number
+}
+
+export interface RespostaPessoas {
+  results: Ator[]
+  total_pages: number
+  total_results: number
+}
+
+export interface FilmeCredito {
+  id: number
+  title: string
+  poster_path: string | null
+  release_date: string
+  vote_average: number
+  character: string
+}
+
+export interface RespostaCreditos {
+  cast: FilmeCredito[]
+}
+
+export interface AvaliacaoUsuario {
+  nota: number
+  opiniao: string
+  data: string
+}
